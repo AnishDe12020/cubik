6aee2cf28659a53cce58d8b09f66cc2c56b36e94
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-08-31 19:59:45 +0530

    feat: init tailwindcss in cubik-ui with correct config so packages/ui works

--- apps/cubik-ui/src/pages/index.tsx
+++ apps/cubik-ui/src/pages/index.tsx
@@ -1,6 +1,7 @@
-import type { GetStaticProps, NextPage } from 'next';
-import LandingPage from '~/components/pages/landing-page/LandingPage';
-import SEO from '~/components/SEO';
+import type { GetStaticProps, NextPage } from "next";
+import LandingPage from "~/components/pages/landing-page/LandingPage";
+import SEO from "~/components/SEO";
+import { Button } from "@cubik/ui";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,8 @@ const Home: NextPage = () => {
         description={`Fund Public Goods Through Community Voting On Solana `}
         image={`https://res.cloudinary.com/demonicirfan/image/upload/v1684179451/cubik%20og.png`}
       />
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <Button className="bg-primary">Button</Button>
     </>
   );
 };

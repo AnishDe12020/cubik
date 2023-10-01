a9fcf9ba4fe1e3f185931864a45f880832d97ca9
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-01 15:01:09 +0530

    chore: test tailwind nextjs project using packages/ui

---
+++ apps/tailwind-test/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+import { Button } from "@cubik/ui";
+
+export default function Home() {
+  return <Button>Hello</Button>;
+  // return (
+  //   <button className="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground hover:bg-primary/90">
+  //     Hello
+  //   </button>
+  // );
+}
